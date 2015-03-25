e4eb84fd0a1c0c2c87ba9adea7d8d53b92edb5bd
jeaye <contact@jeaye.com>
2015-03-25 18:22:04 +0800

    Add ODR tests

---
+++ test/src/odr/main1.cpp
@@ -0,0 +1,15 @@
+/*
+  Copyright © 2014 Jesse 'Jeaye' Wilkerson
+  See licensing at:
+    http://opensource.org/licenses/MIT
+
+  Author: Jesse 'Jeaye' Wilkerson
+*/
+
+#include <jest/jest.hpp>
+
+int main()
+{
+  jest::worker const j{};
+  return j();
+}
---
+++ test/src/odr/main2.cpp
@@ -0,0 +1,9 @@
+/*
+  Copyright © 2014 Jesse 'Jeaye' Wilkerson
+  See licensing at:
+    http://opensource.org/licenses/MIT
+
+  Author: Jesse 'Jeaye' Wilkerson
+*/
+
+#include <jest/jest.hpp>
